15c89537c1fa10065dc2999b30e5920701dbc830
SHTINER <45335680+SHTINER@users.noreply.github.com>
2020-08-23 14:01:31 +0300

    add Person

--- TestProject/src/test/main.java
+++ TestProject/src/test/main.java
@@ -4,6 +4,8 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
+		Person p = new Person("Ivan");
+		System.out.println("Hello, " + p);
 	}
 
 }
